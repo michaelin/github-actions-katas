7b1ae0b46b2a1d43570e54023f7e3dcf791b44d9
Michael Ingeman-Nielsen <michael@i-n.dk>
2025-11-04 00:25:25 +0100

    extend custom action

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello, ${1}!"
+greeting_type="$1"
+who_to_greet="$2"
+
+message="$greeting_type $who_to_greet"
+echo "$message"
+
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
+echo "message=$message" >> $GITHUB_OUTPUT
